cbda4688edc1e4dfc822bc8b35622a0fb8c81ecc
Martin Jeppesen <67677982+swamp2k@users.noreply.github.com>
2026-08-29 15:56:47 +0200

    Load Garmin health styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 import "./garmin.css";
+import "./garmin-health.css";
 import "./weather.css";
 import "./electricity.css";
 import "./settings.css";
