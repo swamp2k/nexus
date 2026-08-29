a772c13c3cae54aff7e69495f0fae5e4fbc0c6bd
Martin Jeppesen <67677982+swamp2k@users.noreply.github.com>
2026-08-29 13:50:59 +0200

    Load weather module styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 import "./garmin.css";
+import "./weather.css";
 import "./kitchen-display.css";
 
 createRoot(document.getElementById("root")!).render(
