ec4a7680f1b6d20eb1a4214e93284857755d4a2f
Martin Jeppesen <67677982+swamp2k@users.noreply.github.com>
2026-09-02 14:52:50 +0200

    Load navigation editor styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "leaflet/dist/leaflet.css";
 import "./styles.css";
+import "./navigation.css";
 import "./home.css";
 import "./home-responsive.css";
 import "./garmin.css";
